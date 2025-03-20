124693d13373c6eef17249248af4f56dda9f708c
Nhi Le <nhile@Nhis-MacBook-Air.local>
2025-03-20 09:43:33 +0100

    searchparams in san-pham page

--- sap-cua-me-fe-final/src/app/san-pham/page.tsx
+++ sap-cua-me-fe-final/src/app/san-pham/page.tsx
@@ -1,13 +1,16 @@
 import { Footer } from "@/components/Footer";
 import { MenuBlock } from "@/components/MenuBar/MenuBlock";
 import { ProductPage } from "@/components/Product/ProductPage";
-import { Stack } from "@chakra-ui/react";
+import { Spinner, Stack } from "@chakra-ui/react";
+import { Suspense } from "react";
 
 export default function AllProductsPage() {
     return (
       <Stack flex="1" gap="10">
             <MenuBlock/>
-            <ProductPage/>
+            <Suspense fallback={<Spinner />}>
+          <ProductPage />
+        </Suspense>
             <Footer />
           </Stack>
     );
